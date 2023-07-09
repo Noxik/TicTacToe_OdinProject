948b9c485f82e837c10533261a69c561079c2f32
Noxik <64279796+Noxik@users.noreply.github.com>
2023-07-09 21:53:02 +0200

    added some divs and CSS, now we can see gameboard, add inputs with player names, added posibility to mark O or X on gameboard after click, fields are button so after click it change to disabled so cannot be clicked once again

--- main.js
+++ main.js
@@ -1,6 +1,53 @@
-(function () {
-    var foo = "bar";
+let playerOneIn = document.getElementById("p1")
+let playerTwoIn = document.getElementById("p2")
+let playerOneName = document.getElementById("player1")
+let playerTwoName = document.getElementById("player2")
 
-    // Outputs: "bar"
-    console.log(foo);
-})();
+const Player = (name) => {
+    const sayName = () => console.log(`Hello ${name}!`)
+    return {sayName}
+}
+
+document.getElementById("start").addEventListener("click", () => {
+    let p1name = document.createElement("p");
+    p1name.textContent = playerOneIn.value;
+    playerOneName.appendChild(p1name)
+
+    let p2name = document.createElement("p");
+    p2name.textContent = playerTwoIn.value;
+    playerTwoName.appendChild(p2name)
+
+    let player1 = Player(playerOneIn.value)
+    player1.sayName();
+    let player2 = Player(playerTwoIn.value)
+    player2.sayName()
+})
+
+const gameboard  =  (()=>{
+    const board = [];
+    return {board}
+})()
+
+
+
+let playerMark = "1"
+//ACCESS TO BOARD DIVs
+const boardDiv = document.querySelectorAll(".field");
+boardDiv.forEach( function(e) {
+    e.addEventListener("click", () => {
+        console.log(e.id, e.textContent)
+   
+    if (e.textContent === "") {
+    e.textContent = playerMark;}
+    
+    if(playerMark === "1") {
+    playerMark = "X"
+    } else {playerMark = "1"}    
+    console.log(playerMark)
+    gameboard.board[e.id-1] = playerMark
+    console.log(gameboard.board)
+    e.disabled = true
+
+    //we need switch players
+}
+    )})
