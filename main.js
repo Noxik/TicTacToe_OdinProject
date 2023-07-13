1e0ebbefef77299bdda5c4c404b9026ed2331a00
Noxik <64279796+Noxik@users.noreply.github.com>
2023-07-13 22:51:42 +0200

    change win validation, only use array with correct marks and check if gameboard have one of 0 combination; short and easy check

--- main.js
+++ main.js
@@ -24,15 +24,11 @@ document.getElementById("start").addEventListener("click", () => {
 })
 
 const gameboard  =  (()=>{
-    const board = [];
+    const board = [1,2,3,4,5,6,7,8,9];
     return {board}
 })()
 
 
-
-let rows = [[],[],[]] // musi byc na zewnatrz! jak jest w srodku to zeruje array
-let cols = [[],[],[]]
-let bothX = [[],[]]
 let playerMark = "O"
 //ACCESS TO BOARD DIVs
 const boardDiv = document.querySelectorAll(".field");
@@ -41,35 +37,7 @@ boardDiv.forEach( function(e) {
 //        console.log(e.id)
     gameboard.board[e.id-1] = playerMark
 
-// SEPARATE ROWS FROM GAMEBOARD
-if (e.id-1 < 3) {
-   rows[0].push(playerMark)
-} else if (e.id-1 < 6) {
-    rows[1].push(playerMark)
-} else {rows[2].push(playerMark)}
-
-
-if ([1,4,7].includes(Number(e.id))) {
-    cols[0].push(playerMark)
-} else if ([2,5,8].includes(Number(e.id))) {
-    cols[1].push(playerMark)
-} else {
-    cols[2].push(playerMark)
-}
-
-if ([1,9].includes(Number(e.id))) {
-    bothX[0].push(playerMark)
-} else if ([3,7].includes(Number(e.id)))
-{bothX[1].push(playerMark)}
-else if (5 === (Number(e.id)))
-{bothX[0].push(playerMark);
-    bothX[1].push(playerMark)}
-    console.log(bothX)
-
-checkWin(rows)
-checkWin(cols)
-checkWin(bothX)
-
+checkWinner()
 
 //change mark after click
     if (e.textContent === "") {
@@ -84,54 +52,17 @@ checkWin(bothX)
     }
 )})
 
-// ROWS DZIELI ARR ZANIM SIE ZAPELNIA, PO KAZDYM DODANIU X LUB O, TRZEBA TO ZMIENIC
-// GAMEBOARD MUSI BYC SPRAWDZANY PO KLIKU CZY JEDEN Z ROW MA 3
+function checkWinner() {
+    let test = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]]
 
-
-// we can use only 1 function to check rows and cols :)
-function checkWin(arr) {
-    for (let i=0; i<arr.length; i++) {
-        if (arr[i].length === 3) {
-        let check = arr[i].every(element => element === arr[i][0])
-        if (check) {
-            console.log(`player ${arr[i][0]} win ${arr[i]}`);
-            boardDiv.forEach(e => e.disabled = true) //DISABLE BUTTONS IF WIN
-                }
-            } 
-        } 
-    } 
-/*
-function checkRows() {
-    for (let i=0; i<rows.length; i++) {
-        if (rows[i].length === 3) {
-        let check = rows[i].every(element => element === rows[i][0])
-        if (check === true) {
-            console.log(`player ${rows[i][0]} win r`);
-            boardDiv.forEach(e => e.disabled = true) //DISABLE BUTTON IF WIN
-                }
-            }
-        }
-    } 
-      
-function checkCols() {
-        for (let i=0; i<cols.length; i++) {
-            if (cols[i].length === 3) {
-            let check = cols[i].every(element => element === cols[i][0])
-            if (check === true) {
-                console.log(`player ${cols[i][0]} win c`);
-                boardDiv.forEach(e => e.disabled = true) //DISABLE BUTTON IF WIN
-                    }
-                }
-            }
-        } 
-    
-
-    /*    for (let i=0; i<9; i++) {
-            if (i<3) {
-                console.log(3)
-        } else if (i<6) {
-            console.log(6)
-        } else {
-        console.log(9)}
-            console.log(gameboard.board)
-        } */
+ for (let i=0; i<test.length; i++) {
+        if (gameboard.board[test[i][0]] == gameboard.board[test[i][1]] && gameboard.board[test[i][0]] == gameboard.board[test[i][2]]) {
+            console.log(test[i]);
+            console.log(gameboard.board[test[i][0]])
+            boardDiv.forEach(e => e.disabled = true)}
+   
+} 
+}    
