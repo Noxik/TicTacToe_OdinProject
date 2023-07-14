bbd39af380a6e06f9130043dc3f798ea5ab754f2
Noxik <64279796+Noxik@users.noreply.github.com>
2023-07-14 23:27:15 +0200

    fix CSS bug change selector of player1 from class to id, add some color: none when fields are disabled, green for winning fields, color for grid cell player turn

--- main.js
+++ main.js
@@ -21,6 +21,8 @@ document.getElementById("start").addEventListener("click", () => {
     player1.sayName();
     let player2 = Player(playerTwoIn.value)
     player2.sayName()
+
+      playerOneName.style.backgroundColor ="#e4a0ff";
 })
 
 const gameboard  =  (()=>{
@@ -28,7 +30,7 @@ const gameboard  =  (()=>{
     return {board}
 })()
 
-
+let turn = 0
 let playerMark = "O"
 //ACCESS TO BOARD DIVs
 const boardDiv = document.querySelectorAll(".field");
@@ -38,6 +40,17 @@ boardDiv.forEach( function(e) {
     gameboard.board[e.id-1] = playerMark
 
 checkWinner()
+
+//added color to player turn
+if (turn %2 !== 0) {
+    playerOneName.style.backgroundColor ="#e4a0ff";
+    playerTwoName.style.backgroundColor ="";
+} else {
+    playerTwoName.style.backgroundColor ="#e4a0ff";
+    playerOneName.style.backgroundColor ="";
+}
+turn++
+
 
 //change mark after click
     if (e.textContent === "") {
@@ -53,16 +66,19 @@ checkWinner()
 )})
 
 function checkWinner() {
-    let test = [
+    let winFields = [
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]]
 
- for (let i=0; i<test.length; i++) {
-        if (gameboard.board[test[i][0]] == gameboard.board[test[i][1]] && gameboard.board[test[i][0]] == gameboard.board[test[i][2]]) {
-            console.log(test[i]);
-            console.log(gameboard.board[test[i][0]])
-            boardDiv.forEach(e => e.disabled = true)}
-   
-} 
+ for (let i=0; i<winFields.length; i++) {
+    if (gameboard.board[winFields[i][0]] == gameboard.board[winFields[i][1]] && gameboard.board[winFields[i][0]] == gameboard.board[winFields[i][2]]) {
+
+// change backgroundcolor of win fields
+    for (let j=0; j<winFields[i].length; j++) {
+        boardDiv[winFields[i][j]].style.backgroundColor = "#7fffd4"
+        }
+ console.log(gameboard.board[winFields[i][0]])
+        boardDiv.forEach(e => e.disabled = true)}
+}
 }    
