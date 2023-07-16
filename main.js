2d24d4ef7d0f4596433c6080797e29d0b27e4940
Noxik <64279796+Noxik@users.noreply.github.com>
2023-07-16 21:56:21 +0200

    added some CSS, add input players before we can see board, add color backgroun  when someone win

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
+let players = document.getElementById("players")
 let playerOneIn = document.getElementById("p1")
 let playerTwoIn = document.getElementById("p2")
-let playerOneName = document.getElementById("player1")
-let playerTwoName = document.getElementById("player2")
+let playerOneName = document.getElementById("player1name")
+let playerTwoName = document.getElementById("player2name")
+let start = document.getElementById("start")
+let mainboard = document.getElementById("mainboard")
+let result = document.getElementById("result")
+
 
 const Player = (name) => {
     const sayName = () => console.log(`Hello ${name}!`)
     return {sayName}
 }
+
+
+start.addEventListener("click", () => {
+players.style.display = "none";
+mainboard.style.display = "grid";
+})
 
 document.getElementById("start").addEventListener("click", () => {
     let p1name = document.createElement("p");
@@ -17,15 +28,15 @@ document.getElementById("start").addEventListener("click", () => {
     p2name.textContent = playerTwoIn.value;
     playerTwoName.appendChild(p2name)
 
-    let player1 = Player(playerOneIn.value)
+    const player1 = Player(playerOneIn.value)
     player1.sayName();
-    let player2 = Player(playerTwoIn.value)
+    const player2 = Player(playerTwoIn.value)
     player2.sayName()
 
-      playerOneName.style.backgroundColor ="#e4a0ff";
+      playerOneName.parentElement.style.backgroundColor ="#25bbf5";
 })
 
-const gameboard  =  (()=>{
+const gameBoard  =  (()=>{
     const board = [1,2,3,4,5,6,7,8,9];
     return {board}
 })()
@@ -37,20 +48,18 @@ const boardDiv = document.querySelectorAll(".field");
 boardDiv.forEach( function(e) {
     e.addEventListener("click", () => {
 //        console.log(e.id)
-    gameboard.board[e.id-1] = playerMark
+    gameBoard.board[e.id-1] = playerMark
 
-checkWinner()
 
 //added color to player turn
 if (turn %2 !== 0) {
-    playerOneName.style.backgroundColor ="#e4a0ff";
-    playerTwoName.style.backgroundColor ="";
+    playerOneName.parentElement.style.backgroundColor ="#25bbf5";
+    playerTwoName.parentElement.style.backgroundColor ="";
 } else {
-    playerTwoName.style.backgroundColor ="#e4a0ff";
-    playerOneName.style.backgroundColor ="";
+    playerTwoName.parentElement.style.backgroundColor ="#25bbf5";
+    playerOneName.parentElement.style.backgroundColor ="";
 }
 turn++
-
 
 //change mark after click
     if (e.textContent === "") {
@@ -58,11 +67,10 @@ turn++
     if(playerMark === "O") {
     playerMark = "X"
     } else {playerMark = "O"}    
-//    console.log(playerMark)
-//    console.log(gameboard.board)
     e.disabled = true
     //we need switch players
-    }
+    checkWinner()
+}
 )})
 
 function checkWinner() {
@@ -70,15 +78,38 @@ function checkWinner() {
         [0,1,2],[3,4,5],[6,7,8],
         [0,3,6],[1,4,7],[2,5,8],
         [0,4,8],[2,4,6]]
+    
+    let winner
 
- for (let i=0; i<winFields.length; i++) {
-    if (gameboard.board[winFields[i][0]] == gameboard.board[winFields[i][1]] && gameboard.board[winFields[i][0]] == gameboard.board[winFields[i][2]]) {
+    for (let i=0; i<winFields.length; i++) {
+        if (gameBoard.board[winFields[i][0]] == gameBoard.board[winFields[i][1]] 
+        && gameBoard.board[winFields[i][0]] == gameBoard.board[winFields[i][2]]) {
 
-// change backgroundcolor of win fields
+// change backgroundcolor of win fields (under O or X marks)
     for (let j=0; j<winFields[i].length; j++) {
         boardDiv[winFields[i][j]].style.backgroundColor = "#7fffd4"
         }
- console.log(gameboard.board[winFields[i][0]])
-        boardDiv.forEach(e => e.disabled = true)}
+        winner = gameBoard.board[winFields[i][0]];
+        boardDiv.forEach(e => e.disabled = true);  
+
+        if (winner === "O") {
+            result.textContent = `Player 1 ${playerOneIn.value} won!!`;
+            playerOneName.parentElement.style.backgroundColor ="#7fffd4"
+            playerTwoName.parentElement.style.backgroundColor =""
+        } else {
+            result.textContent = `Player 2 ${playerTwoIn.value} won!!`;
+            playerTwoName.parentElement.style.backgroundColor ="#7fffd4"
+            playerOneName.parentElement.style.backgroundColor =""
+        }
+    }
 }
+
+    if (gameBoard.board.every(elem => ["O","X"].indexOf(elem) > -1) 
+        && winner === undefined) {
+    console.log("tie");
+    result.textContent = "It's Tie! Play again :)";
+    playerTwoName.parentElement.style.backgroundColor ="";
+    playerOneName.parentElement.style.backgroundColor ="";
+        }
+
 }    
