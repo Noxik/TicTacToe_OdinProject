b192173adf9ef0b80d28aaf6416c3fcbe072cba7
Noxik <64279796+Noxik@users.noreply.github.com>
2023-07-21 22:44:28 +0200

    added some html/css for additional game mode vs AI, added easy script to AI move after player takes one of the fields

--- main.js
+++ main.js
@@ -11,11 +11,15 @@ let playerTwoIn = document.getElementById("p2")
 let playerOneDiv = document.getElementById("player1name")
 let playerTwoDiv = document.getElementById("player2name")
 
+//AI
+let playerAiIn = document.getElementById("p1vai")
+
+let whoPlay
 //create board   
     const board = [1,2,3,4,5,6,7,8,9];
 
 //start players setting
-    document.getElementById("start").addEventListener("click", () => {
+    document.getElementById("startpvp").addEventListener("click", () => {
         document.getElementById("players").style.display = "none";
         document.getElementById("mainboard").style.display = "grid";
         let p1name = document.createElement("p");
@@ -32,9 +36,33 @@ let playerTwoDiv = document.getElementById("player2name")
         player2.sayName()
     
         playerOneDiv.parentElement.style.backgroundColor ="#25bbf5";
+
+        gameBoard.whoPlay = "pvp"
     })
 
-    return {board, playerOneIn, playerTwoIn, playerOneDiv, playerTwoDiv}
+//AI start settings
+document.getElementById("startpve").addEventListener("click", () => {
+    document.getElementById("players").style.display = "none";
+    document.getElementById("mainboard").style.display = "grid";
+    let p1name = document.createElement("p");
+    p1name.textContent = playerAiIn.value;
+    playerOneDiv.appendChild(p1name)
+
+    let p2name = document.createElement("p");
+    p2name.textContent = `AI level: ${document.querySelector("select").value}`;
+    playerTwoDiv.appendChild(p2name)
+
+    const player1 = Player(playerAiIn.value)
+    player1.sayName();
+    const player2 = Player(document.querySelector("select").value)
+    player2.sayName()
+
+    playerOneDiv.parentElement.style.backgroundColor ="#25bbf5";
+    
+    gameBoard.whoPlay = "pve"
+})
+
+    return {board, playerOneIn, playerTwoIn, playerOneDiv, playerTwoDiv, whoPlay}
 })()
 
 const displayController = (() => {
@@ -45,37 +73,50 @@ const boardDiv = document.querySelectorAll(".field");
 
 boardDiv.forEach( function(e) {
     e.addEventListener("click", () => {
+    
 //        console.log(e.id)
     gameBoard.board[e.id-1] = displayController.playerMark
-
+console.log(displayController.turn)
 //added color to player turn
+  
     if (displayController.turn %2 !== 0) {
         gameBoard.playerOneDiv.parentElement.style.backgroundColor ="#25bbf5";
         gameBoard.playerTwoDiv.parentElement.style.backgroundColor ="";
         gameFlow.result.textContent = "Now O turn"
     } else {
-        gameBoard.playerTwoDiv.parentElement.style.backgroundColor ="#25bbf5";
         gameBoard.playerOneDiv.parentElement.style.backgroundColor ="";
+        gameBoard.playerTwoDiv.parentElement.style.backgroundColor ="#25bbf5";
         gameFlow.result.textContent = "Now X turn"
     }
-        displayController.turn++
-
+        
     //change mark after click
         if (e.textContent === "") {
         e.textContent = displayController.playerMark;}
         if(displayController.playerMark === "O") {
-            displayController.playerMark = "X"
+            displayController.playerMark = "X";    
         } else {displayController.playerMark = "O"}    
         e.disabled = true
         //we need switch players
+        
+        displayController.turn++
+
         gameFlow.checkWinner()
+
+    //AI move after click
+    if (gameBoard.whoPlay === "pve" && gameFlow.winner === undefined) {         
+            setTimeout(() => {
+                test();
+              }, 500);
+        }
         }
     )})
-
+    
 return {boardDiv, playerMark, turn}
 })()
 
 const gameFlow = (() => {
+
+let winner
 
 document.getElementById("playAgain").addEventListener("click", playAgain)
     let result = document.getElementById("result")
@@ -85,7 +126,7 @@ document.getElementById("playAgain").addEventListener("click", playAgain)
             [0,3,6],[1,4,7],[2,5,8],
             [0,4,8],[2,4,6]]
         
-        let winner
+       
     
         for (let i=0; i<winFields.length; i++) {
             if (gameBoard.board[winFields[i][0]] == gameBoard.board[winFields[i][1]] 
@@ -95,10 +136,10 @@ document.getElementById("playAgain").addEventListener("click", playAgain)
         for (let j=0; j<winFields[i].length; j++) {
             displayController.boardDiv[winFields[i][j]].style.backgroundColor = "#7fffd4"
             }
-            winner = gameBoard.board[winFields[i][0]];
+            gameFlow.winner = gameBoard.board[winFields[i][0]];
             displayController.boardDiv.forEach(e => e.disabled = true);  
     
-            if (winner === "O") {
+            if (gameFlow.winner === "O") {
                 result.textContent = `Player O ${gameBoard.playerOneIn.value} won!!`;
                 gameBoard.playerOneDiv.parentElement.style.backgroundColor ="#7fffd4"
                 gameBoard.playerTwoDiv.parentElement.style.backgroundColor =""
@@ -111,7 +152,7 @@ document.getElementById("playAgain").addEventListener("click", playAgain)
     }
     
         if (gameBoard.board.every(elem => ["O","X"].indexOf(elem) > -1) 
-            && winner === undefined) {
+            && gameFlow.winner === undefined) {
         console.log("tie");
         result.textContent = "It's Tie! Play again :)";
         gameBoard.playerTwoDiv.parentElement.style.backgroundColor ="";
@@ -120,7 +161,7 @@ document.getElementById("playAgain").addEventListener("click", playAgain)
     }    
     
     function playAgain() {
-        winner = undefined
+        gameFlow.winner = undefined
         gameBoard.board = [1,2,3,4,5,6,7,8,9];
     
         // clear players name divs
@@ -139,12 +180,38 @@ document.getElementById("playAgain").addEventListener("click", playAgain)
             displayController.turn = 1;
             gameBoard.playerTwoDiv.parentElement.style.backgroundColor ="#25bbf5";
             result.textContent = "Now X turn"
+         
+        // AI move after play again if O win or started previous game
+            setTimeout(() => {
+                test();
+              }, 1000);
+        
         } else {
             displayController.turn = 0;
             gameBoard.playerOneDiv.parentElement.style.backgroundColor ="#25bbf5";
             result.textContent = "Now O turn"
         }
     }
-    
-return {playAgain, checkWinner, result}
+
+return {playAgain, checkWinner, result, winner}
 })()
+
+function test() {
+    const random = Math.floor(Math.random() * gameBoard.board.length);
+        
+        if (["X", "O"].includes(gameBoard.board[random]) === true) {
+        //    console.log(random, gameBoard.board[random]);
+            test ()
+        } else {
+        //    console.log(random, gameBoard.board[random]);
+            gameBoard.board[random] = "X"
+            displayController.boardDiv[random].textContent = "X";
+            displayController.boardDiv[random].disabled = true;
+            displayController.playerMark = "O";
+            displayController.turn++;
+            gameBoard.playerTwoDiv.parentElement.style.backgroundColor =""
+            gameBoard.playerOneDiv.parentElement.style.backgroundColor ="#25bbf5"
+        }
+        gameFlow.checkWinner()
+
+    }
